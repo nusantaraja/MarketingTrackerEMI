4fbd02da5b4e07ae8aaedeb57933e06c4543e791
nusantaraja <iwan_cahyo@yahoo.com>
2025-06-07 00:38:28 +0700

    edit core v.37

--- app_with_edit_delete.py
+++ app_with_edit_delete.py
@@ -126,7 +126,10 @@ def show_login_page():
             submit = st.form_submit_button("Login", use_container_width=True)
             
             if submit:
-                if authenticate_user(username, password):
+                user = authenticate_user(username, password)
+                if user:
+                    st.session_state.logged_in = True
+                    st.session_state.user = user
                     st.success("Login berhasil!")
                     st.rerun()
                 else:
